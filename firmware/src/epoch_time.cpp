88902daa8fa36ff1cfb9c3f7d2fcfa1d3865fc8e
sKrible45 <al144hardy@outlook.com>
2025-08-14 09:04:57 +0100

    Refactored epoch time function.

--- firmware/src/epoch_time.cpp
+++ firmware/src/epoch_time.cpp
@@ -1,30 +1,19 @@
-#include <NTPClient.h>
 #include <WiFi.h>
+#include "time.h"
 
-WiFiUDP ntpUDP;
-NTPClient timeClient(ntpUDP);
+const char* ntpServer = "pool.ntp.org";
 
 // Get the epoch time from an ntp server (returns 0 when an error has occured)
 unsigned long GetEpoch(){
-    unsigned long currentTime;
-
-    const int forceUpdateLimit = 10;
-    int forceUpdates = 0;
-    timeClient.begin();
-    while(!timeClient.update() && forceUpdates <= forceUpdateLimit) {
-        // Try and force the timeclient update.
-        timeClient.forceUpdate();
-        Serial.println("Timeclient not updated, forcing update.");
-        forceUpdates ++;
-        delay(1000);
-    }
-    if (forceUpdates > forceUpdateLimit){
+    configTime(0, 0, ntpServer);
+    struct tm timeinfo;
+    if (!getLocalTime(&timeinfo)){
         return 0;
     }
-    else{
-        currentTime = timeClient.getEpochTime();
-    }
+    time_t epoch;
+    time(&epoch);
+    Serial.print("Epoch: ");
+    Serial.println(epoch);
 
-
-    return currentTime;
+    return epoch;
 }
